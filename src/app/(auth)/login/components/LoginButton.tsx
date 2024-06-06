8956e36ecd961d804b2790e808522ac84d71cf1f
RinGoku <skyta80105@gmail.com>
2024-06-06 15:06:54 +0900

    login dynamic import

---
+++ src/app/(auth)/login/components/LoginButton.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { supabase } from '@/lib/supabase/client'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+export const LoginButton = () => {
+  return (
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      providers={['google']}
+      redirectTo={
+        typeof window !== undefined ? `/api/auth/callback` : undefined
+      }
+      localization={{
+        variables: {
+          sign_in: {
+            email_label: 'メールアドレス',
+            email_input_placeholder: 'メールアドレス',
+            password_label: 'パスワード',
+            password_input_placeholder: 'パスワード',
+            button_label: 'ログイン',
+            link_text: 'アカウントをお持ちの場合はこちら',
+          },
+          sign_up: {
+            email_label: 'メールアドレス',
+            email_input_placeholder: 'メールアドレス',
+            password_label: 'パスワード',
+            password_input_placeholder: 'パスワード',
+            button_label: 'ログイン',
+            link_text: 'アカウントをお持ちでない場合はこちら',
+          },
+          forgotten_password: {
+            email_label: 'メールアドレス',
+            email_input_placeholder: 'メールアドレス',
+            link_text: 'パスワードをお忘れですか？',
+            button_label: 'パスワードをリセット',
+          },
+        },
+      }}
+    />
+  )
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,47 +1,17 @@
 'use client'
-import { supabase } from '@/lib/supabase/client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+const LoginButton = dynamic(
+  () =>
+    import('@/app/(auth)/login/components/LoginButton').then(
+      (module) => module.LoginButton,
+    ) as any,
+  { ssr: false },
+)
+import dynamic from 'next/dynamic'
 import styled from './page.module.css'
 export default function Login() {
   return (
     <main className={styled.wrapper}>
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={['google']}
-        redirectTo={
-          window !== undefined
-            ? `${window.location.origin}/api/auth/callback`
-            : undefined
-        }
-        localization={{
-          variables: {
-            sign_in: {
-              email_label: 'メールアドレス',
-              email_input_placeholder: 'メールアドレス',
-              password_label: 'パスワード',
-              password_input_placeholder: 'パスワード',
-              button_label: 'ログイン',
-              link_text: 'アカウントをお持ちの場合はこちら',
-            },
-            sign_up: {
-              email_label: 'メールアドレス',
-              email_input_placeholder: 'メールアドレス',
-              password_label: 'パスワード',
-              password_input_placeholder: 'パスワード',
-              button_label: 'ログイン',
-              link_text: 'アカウントをお持ちでない場合はこちら',
-            },
-            forgotten_password: {
-              email_label: 'メールアドレス',
-              email_input_placeholder: 'メールアドレス',
-              link_text: 'パスワードをお忘れですか？',
-              button_label: 'パスワードをリセット',
-            },
-          },
-        }}
-      />
+      <LoginButton />
     </main>
   )
 }
